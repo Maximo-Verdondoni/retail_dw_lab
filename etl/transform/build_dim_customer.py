48c92599193cb9009673619a690b90024f5979ce
Maximo-Verdondoni <avryux04@gmail.com>
2025-10-08 23:35:13 -0300

    feat(dim_customer): se creo dim_customer uniendo customer con customer_addresses

--- etl/transform/build_dim_customer.py
+++ etl/transform/build_dim_customer.py
@@ -29,7 +29,41 @@ def build_dim_customer(data: dict, output_path):
     ]
     dim_customer = dim_customer[cols]
 
-    
+
+    # -------------------
+
+    #Cargamos customer_addreses 
+    addresses = data["customer_addresses"].copy()
+
+    #Hacemos el join
+    dim_customer = pd.merge(
+        dim_customer,
+        addresses,
+        on='customer_id',
+        how='left'
+    )
+    cols = [
+        "customer_id", "first_name", "last_name", "email", "phone", "gender",
+        "birth_date", "created_at_date_id", "created_at_time", "marketing_opt_in",
+        "address_type", "street", "city", "province", "country_code", "postal_code",
+        "created_at", "is_primary"
+    ]
+    dim_customer = dim_customer[cols]
+
+    #Separamos created_at_address en fecha y hora
+    dim_customer['created_at'] = pd.to_datetime(dim_customer['created_at'])
+    dim_customer['created_at_date_address'] = dim_customer['created_at'].dt.date
+    dim_customer['created_at_time_address'] = dim_customer['created_at'].dt.time
+    dim_customer = dim_customer.drop(columns=['created_at'])
+
+    #Reordenamos columnas
+    cols = [
+        "customer_id", "first_name", "last_name", "email", "phone", "gender",
+        "birth_date", "created_at_date_id", "created_at_time", "marketing_opt_in",
+        "address_type", "street", "city", "province", "country_code", "postal_code",
+        "created_at_date_address", "created_at_time_address", "is_primary"
+    ]
+    dim_customer = dim_customer[cols]    
     # salida en warehouse/dim
     file_path = output_path / "dim" / "dim_customers.csv"
     file_path.parent.mkdir(parents=True, exist_ok=True)  # aseguro que exista la carpeta
